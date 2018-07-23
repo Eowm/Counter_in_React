e72071115522841909d00be32dc8c3fa456741f0
Adam Nelip <Adam.Nelip@gmail.com>
2018-07-23 17:43:29 +0200

    added component life cycle methods

--- components/Counter.js
+++ components/Counter.js
@@ -17,6 +17,28 @@ var Counter = React.createClass({
         });
     },
 
+    getDefaultProps() {
+       console.log('this method is used when we want to initialize default component props.');
+    },
+    componentWillMount() {
+        console.log('This method starts before render component. Prepare component to render.');
+    },
+    componentDidMount() {
+        console.log('This method starts just after start component. We can download some data, do some change on the component, change state component etc.');
+    },
+    componentWillUnmount() {
+        console.log('This method start when component is delete from DOM.');
+    },
+    componentWillReceiveProps(nextProps) {
+        console.log('This method start when we initialize component in parent component and we set some properties for this child component.');
+    },
+    componentWillUpdate(nextProps, nextState) {
+        console.log('This method start after shouldComponentUpdate method if get "true". This method prepare component to some change. If we set new property or state');
+    },
+    componentDidUpdate(prevProps, prevState) {
+        console.log('This method start after render method. In this method we can change something in props or state');
+    },
+
     render: function(){
     	return (
     		React.createElement('div', {className: 'counter'},
